fd914785c10880df6ea68f0f55d6a9186c079e46
chensiqin123 <11422289+chensiqin123@user.noreply.gitee.com>
2026-01-14 15:21:02 +0800

    Initial commit

---
+++ src/main/java/org/lpd/devicemanagermqtt/DeviceManagerMqttApplication.java
@@ -0,0 +1,13 @@
+package org.lpd.devicemanagermqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagerMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagerMqttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lpd/devicemanagermqtt/DeviceManagerMqttApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lpd.devicemanagermqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerMqttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
